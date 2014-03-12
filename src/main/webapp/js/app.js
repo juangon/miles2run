b689e52bbfdd6e5161ea34653b976e6c8d618f47
Shekhar Gulati <shekhargulati84@gmail.com>
2014-03-12 19:17:36 +0530

    fixed https://github.com/shekhargulati/milestogo/issues/37

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-angular.module("milestogo.services", ["ngResource"]).
+angular.module("milestogo.services", ["ngResource","ui.bootstrap"]).
     factory('Activity', function ($resource) {
         var Activity = $resource('api/v1/activities/:activityId', {activityId: '@id'});
         Activity.prototype.isNew = function () {
@@ -38,6 +38,45 @@ function ActivityCreateController($scope, $routeParams, $location, $http, Activi
             $location.path('/');
         });
     };
+
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.showWeeks = true;
+    $scope.toggleWeeks = function () {
+        $scope.showWeeks = ! $scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = ( $scope.minDate ) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yy'",
+        'starting-day': 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+    $scope.format = $scope.formats[0];
 }
 
 
