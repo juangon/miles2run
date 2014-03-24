e4b0b71a9e7b6271b30ba81c925f756dc40a4195
Shekhar Gulati <shekhargulati84@gmail.com>
2014-03-25 00:48:03 +0530

    fixed https://github.com/shekhargulati/milestogo/issues/79

--- src/main/java/org/milestogo/signin/TwitterCallbackServlet.java
+++ src/main/java/org/milestogo/signin/TwitterCallbackServlet.java
@@ -1,5 +1,6 @@
 package org.milestogo.signin;
 
+import org.apache.commons.lang3.StringUtils;
 import org.milestogo.domain.SocialConnection;
 import org.milestogo.domain.SocialProvider;
 import org.milestogo.services.ProfileService;
@@ -14,7 +15,10 @@ import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -35,53 +39,59 @@ public class TwitterCallbackServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Twitter twitter = twitterFactory.getInstance();
-        HttpSession session = request.getSession();
-        RequestToken requestToken = (RequestToken) session.getAttribute("requestToken");
-        String oauthVerifier = request.getParameter("oauth_verifier");
-        String connectionId = null;
-        ServletContext servletContext = request.getServletContext();
-        try {
-            if (oauthVerifier != null && requestToken == null) {
-                logger.info(String.format("Request token was %s. So fetching it from ServletContext", requestToken));
-                String twitterSentRequestToken = request.getParameter("oauth_token");
-                if (twitterSentRequestToken != null) {
-                    requestToken = (RequestToken) servletContext.getAttribute(twitterSentRequestToken);
+        String denied = request.getParameter("denied");
+        if (StringUtils.isNotBlank(denied)) {
+            response.sendRedirect(request.getContextPath());
+        } else {
+            Twitter twitter = twitterFactory.getInstance();
+            HttpSession session = request.getSession();
+            RequestToken requestToken = (RequestToken) session.getAttribute("requestToken");
+            String oauthVerifier = request.getParameter("oauth_verifier");
+            String connectionId = null;
+            ServletContext servletContext = request.getServletContext();
+            try {
+                if (oauthVerifier != null && requestToken == null) {
+                    logger.info(String.format("Request token was %s. So fetching it from ServletContext", requestToken));
+                    String twitterSentRequestToken = request.getParameter("oauth_token");
+                    if (twitterSentRequestToken != null) {
+                        requestToken = (RequestToken) servletContext.getAttribute(twitterSentRequestToken);
+                    }
+                    if (requestToken == null) {
+                        throw new IllegalStateException("Verifier present but request token null");
+                    }
+                    //Discard the stored request tokens
+                    servletContext.removeAttribute(twitterSentRequestToken);
                 }
-                if (requestToken == null) {
-                    throw new IllegalStateException("Verifier present but request token null");
-                }
-                //Discard the stored request tokens
-                servletContext.removeAttribute(twitterSentRequestToken);
-            }
-            logger.info("Request Token " + requestToken);
-            AccessToken oAuthAccessToken = twitter.getOAuthAccessToken(requestToken, oauthVerifier);
-            session.removeAttribute("requestToken");
-            connectionId = String.valueOf(twitter.getId());
-            SocialConnection existingSocialConnection = socialConnectionService.findByConnectionId(connectionId);
-            logger.info("SocialConnection " + existingSocialConnection);
-            if (existingSocialConnection != null) {
-                if (existingSocialConnection.getProfile() == null) {
-                    logger.info("Profile was null. So redirecting to new profile creation.");
-                    response.sendRedirect(request.getContextPath() + "/profiles/new?connectionId=" + connectionId);
+                logger.info("Request Token " + requestToken);
+                AccessToken oAuthAccessToken = twitter.getOAuthAccessToken(requestToken, oauthVerifier);
+                session.removeAttribute("requestToken");
+                connectionId = String.valueOf(twitter.getId());
+                SocialConnection existingSocialConnection = socialConnectionService.findByConnectionId(connectionId);
+                logger.info("SocialConnection " + existingSocialConnection);
+                if (existingSocialConnection != null) {
+                    if (existingSocialConnection.getProfile() == null) {
+                        logger.info("Profile was null. So redirecting to new profile creation.");
+                        response.sendRedirect(request.getContextPath() + "/profiles/new?connectionId=" + connectionId);
+                    } else {
+                        String username = existingSocialConnection.getProfile().getUsername();
+                        logger.info(String.format("User %s already had authenticated with twitter. So redirecting to home.", username));
+                        HttpSession newSession = request.getSession(true);
+                        logger.info(String.format("New session created with id %s", newSession.getId()));
+                        newSession.setAttribute("username", username);
+                        newSession.setAttribute("connectionId", connectionId);
+                        response.sendRedirect(request.getContextPath() + "/home");
+                    }
                 } else {
-                    String username = existingSocialConnection.getProfile().getUsername();
-                    logger.info(String.format("User %s already had authenticated with twitter. So redirecting to home.", username));
-                    HttpSession newSession = request.getSession(true);
-                    logger.info(String.format("New session created with id %s", newSession.getId()));
-                    newSession.setAttribute("username", username);
-                    newSession.setAttribute("connectionId", connectionId);
-                    response.sendRedirect(request.getContextPath() + "/home");
+                    SocialConnection socialConnection = new SocialConnection(oAuthAccessToken.getToken(), oAuthAccessToken.getTokenSecret(), SocialProvider.TWITTER, oAuthAccessToken.getScreenName(), connectionId);
+                    socialConnectionService.save(socialConnection);
+                    response.sendRedirect(request.getContextPath() + "/profiles/new?connectionId=" + connectionId);
                 }
-            } else {
-                SocialConnection socialConnection = new SocialConnection(oAuthAccessToken.getToken(), oAuthAccessToken.getTokenSecret(), SocialProvider.TWITTER, oAuthAccessToken.getScreenName(), connectionId);
-                socialConnectionService.save(socialConnection);
-                response.sendRedirect(request.getContextPath() + "/profiles/new?connectionId=" + connectionId);
-            }
 
-        } catch (TwitterException e) {
-            throw new RuntimeException("Not able to make handshake with Twitter. Reason is: ", e);
+            } catch (TwitterException e) {
+                throw new RuntimeException("Not able to make handshake with Twitter. Reason is: ", e);
+            }
         }
+
 
     }
 }
