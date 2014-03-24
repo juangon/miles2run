af0d11ea49b1bf07458839b31a5a8c4f4e8ee5c1
Shekhar Gulati <shekhargulati84@gmail.com>
2014-03-24 16:29:20 +0530

    share button should only be enabled when providers checkbox are selected

--- src/main/webapp/scripts/controllers/ShareActivity.js
+++ src/main/webapp/scripts/controllers/ShareActivity.js
@@ -29,6 +29,22 @@ angular.module('milestogo')
                 toastr.error("Unable to share activity. Please try later.");
                 $location.path('/');
             });
+        };
+
+        $scope.isDisabled = function () {
+            console.log($scope.activityDetails);
+            if(!$scope.activityDetails){
+                return "disabled";
+            }
+            return anyProviderSelected($scope.activityDetails.share) ? "goodtogo" : "disabled";
         }
+
+        var anyProviderSelected = function(share){
+            if(share.facebook || share.twitter || share.googlePlus){
+                return true;
+            }
+            return false;
+        }
+
 
     });
